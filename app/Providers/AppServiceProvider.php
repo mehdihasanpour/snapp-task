782d3ebb8ccaf730ecca8773834a5d99c98f913a
Mehdi Hasanpour <me.hasani1367@gmail.com>
2023-08-24 23:33:22 +0330

    bind sql report to report interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\TransactionRepositoryInterface;
+use App\Repositories\SqlTransactionRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TransactionRepositoryInterface::class, SqlTransactionRepository::class);
     }
 
     /**
