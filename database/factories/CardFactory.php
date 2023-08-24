b843b13a8f8e8cf6593141a6d3ba2a3fc23c2c14
Mehdi Hasanpour <me.hasani1367@gmail.com>
2023-08-24 23:24:35 +0330

    refactor card factory

--- database/factories/CardFactory.php
+++ database/factories/CardFactory.php
@@ -14,10 +14,18 @@ class CardFactory extends Factory
     {
         return [
             'account_id' => Account::factory(),
-            'card_number' => $this->faker->unique()->creditCardNumber,
+            'card_number' => $this->faker->regexify('/^(603799)\d{10}$/'),
             'expiration_date' => $this->faker->creditCardExpirationDate,
             'cvv' => $this->faker->numerify('###'),
             'balance' => $this->faker->numberBetween(0, 10000000),
         ];
+    }
+
+    protected function generateCardNumber()
+    {
+        $prefix = '603799';
+        $suffix = $this->faker->numberBetween(10000000, 99999999); // Generate an 8-digit suffix
+
+        return $prefix . $suffix;
     }
 }
