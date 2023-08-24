9fe8a6433b2c860a946c4aee6c245fb1d5dd9344
Mehdi Hasanpour <me.hasani1367@gmail.com>
2023-08-24 23:24:56 +0330

    use lang file for messages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'transfer' => [
+        'success' => 'Money transferred successfully',
+        'fail' => 'Transfer failed',
+        'insufficient-balance' => 'Your balance is insufficient',
+    ],
+];
