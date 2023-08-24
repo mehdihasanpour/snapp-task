b93b44d8bc21545ed92d70f4987fa9d5eb45c7b7
Mehdi Hasanpour <me.hasani1367@gmail.com>
2023-08-25 00:51:48 +0330

    use service for transaction and refactor controller

--- app/Http/Controllers/V1/TransactionController.php
+++ app/Http/Controllers/V1/TransactionController.php
@@ -8,15 +8,18 @@ use App\Models\Wage;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MoneyTransferRequest;
 use App\Jobs\SendSmsNotification;
+use App\Services\Transaction\TransactionService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
-    public function __invoke(MoneyTransferRequest $request)
+    public function __invoke(MoneyTransferRequest $request, TransactionService $transactionService)
     {
-        $sourceCard = Card::where('card_number', $request->input('source_card_number'))->first();
-        $destinationCard = Card::where('card_number', $request->input('destination_card_number'))->first();
+        DB::beginTransaction();
+
+        $sourceCard = Card::where('card_number', $request->input('source_card_number'))->lockForUpdate()->first();
+        $destinationCard = Card::where('card_number', $request->input('destination_card_number'))->lockForUpdate()->first();
 
         $amount = $request->input('amount');
 
@@ -24,37 +27,10 @@ class TransactionController extends Controller
             return response()->json(['message' => __('messages.transfer.insufficient-balance')], JsonResponse::HTTP_BAD_REQUEST);
         }
 
-        DB::beginTransaction();
-
-        try {
-            $sourceCard->decrement('balance', $amount + Wage::AMOUNT);
-            $destinationCard->increment('balance', $amount);
-
-            $transaction = new Transaction([
-                'amount' => $amount,
-                'transaction_date' => now(),
-            ]);
-            $transaction->sourceCard()->associate($sourceCard);
-            $transaction->destinationCard()->associate($destinationCard);
-            $transaction->save();
-
-            $wage = new Wage([
-                'wage_amount' => Wage::AMOUNT,
-                'wage_date' => now(),
-            ]);
-            $wage->transaction()->associate($transaction);
-            $wage->save();
-
-            dispatch(new SendSmsNotification($sourceCard->account->user->phone, 'Your transaction has been processed.'));
-            dispatch(new SendSmsNotification($destinationCard->account->user->phone, 'You have received a transaction.'));
-
-            DB::commit();
-
+        if ($transactionService->transferMoney($sourceCard, $destinationCard, $amount)) {
             return response()->json(['message' => __('messages.transfer.success')], JsonResponse::HTTP_OK);
-        } catch (\Exception $e) {
-            DB::rollback();
-            return response()->json(['message' => __('messages.transfer.fail')], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
 
+        return response()->json(['message' => __('messages.transfer.fail')], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
---
+++ app/Services/Transaction/TransactionService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services\Transaction;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\Card;
+use App\Models\Transaction;
+use App\Models\Wage;
+use App\Jobs\SendSmsNotification;
+
+class TransactionService
+{
+    public function transferMoney(Card $sourceCard, Card $destinationCard, $amount)
+    {
+        try {
+            $sourceCard->decrement('balance', $amount + Wage::AMOUNT);
+            $destinationCard->increment('balance', $amount);
+
+            $transaction = new Transaction([
+                'amount' => $amount,
+                'transaction_date' => now(),
+            ]);
+            $transaction->sourceCard()->associate($sourceCard);
+            $transaction->destinationCard()->associate($destinationCard);
+            $transaction->save();
+
+            $wage = new Wage([
+                'wage_amount' => Wage::AMOUNT,
+                'wage_date' => now(),
+            ]);
+            $wage->transaction()->associate($transaction);
+            $wage->save();
+
+            dispatch(new SendSmsNotification($sourceCard->account->user->phone, 'Your transaction has been processed.'));
+            dispatch(new SendSmsNotification($destinationCard->account->user->phone, 'You have received a transaction.'));
+
+            DB::commit();
+
+            return true;
+        } catch (\Exception $e) {
+            DB::rollback();
+            return false;
+        }
+    }
+}
