2dd56e1512046a383faf6ddd93425aed792cd4ca
Mehdi Hasanpour <me.hasani1367@gmail.com>
2023-08-25 17:09:38 +0330

    refactor controller and service

--- app/Http/Controllers/V1/TransactionController.php
+++ app/Http/Controllers/V1/TransactionController.php
@@ -2,33 +2,16 @@
 
 namespace App\Http\Controllers\V1;
 
-use App\Models\Card;
-use App\Models\Wage;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MoneyTransferRequest;
 use App\Services\Transaction\TransactionService;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
     public function __invoke(MoneyTransferRequest $request, TransactionService $transactionService)
     {
-        DB::beginTransaction();
+        $transferResult = $transactionService->transferMoney($request->input('source_card_number'), $request->input('destination_card_number'), $request->input('amount'));
 
-        $sourceCard = Card::where('card_number', $request->input('source_card_number'))->lockForUpdate()->first();
-        $destinationCard = Card::where('card_number', $request->input('destination_card_number'))->lockForUpdate()->first();
-
-        $amount = $request->input('amount');
-
-        if ($sourceCard->balance < $amount + Wage::AMOUNT) {
-            return response()->json(['message' => __('messages.transfer.insufficient-balance')], JsonResponse::HTTP_BAD_REQUEST);
-        }
-
-        if ($transactionService->transferMoney($sourceCard, $destinationCard, $amount)) {
-            return response()->json(['message' => __('messages.transfer.success')], JsonResponse::HTTP_OK);
-        }
-
-        return response()->json(['message' => __('messages.transfer.fail')], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        return response()->json(['message' => $transferResult['message']], $transferResult['status']);
     }
 }
--- app/Services/Transaction/TransactionService.php
+++ app/Services/Transaction/TransactionService.php
@@ -7,13 +7,23 @@ use Illuminate\Support\Facades\DB;
 use App\Models\Card;
 use App\Models\Transaction;
 use App\Models\Wage;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
 
 class TransactionService
 {
-    public function transferMoney(Card $sourceCard, Card $destinationCard, $amount)
+    public function transferMoney(string $sourceCardNumber, string $destinationCardNumber, $amount)
     {
+        DB::beginTransaction();
+
         try {
+            $sourceCard = Card::where('card_number', $sourceCardNumber)->lockForUpdate()->first();
+            $destinationCard = Card::where('card_number', $destinationCardNumber)->lockForUpdate()->first();
+
+            if ($sourceCard->balance < $amount + Wage::AMOUNT) {
+                return ['message' => __('messages.transfer.insufficient-balance'), 'status' => JsonResponse::HTTP_BAD_REQUEST];
+            }
+
             $sourceCard->decrement('balance', $amount + Wage::AMOUNT);
             $sourceCard->account->decrement('current_balance', $amount + Wage::AMOUNT);
             $destinationCard->increment('balance', $amount);
@@ -28,16 +38,16 @@ class TransactionService
             $wage->transaction()->associate($transaction);
             $wage->save();
 
-            dispatch(new SendSms($sourceCard->account->user->phone, __('messages.sms.source',['amount'=>$amount])))->afterCommit();
-            dispatch(new SendSms($destinationCard->account->user->phone,  __('messages.sms.destination',['amount'=>$amount])))->afterCommit();
+            dispatch(new SendSms($sourceCard->account->user->phone, __('messages.sms.source', ['amount' => $amount])))->afterCommit();
+            dispatch(new SendSms($destinationCard->account->user->phone,  __('messages.sms.destination', ['amount' => $amount])))->afterCommit();
 
             DB::commit();
 
-            return true;
+            return ['message' => __('messages.transfer.success'), 'status' => JsonResponse::HTTP_OK];
         } catch (\Exception $e) {
             Log::error('Transfer error: ' . $e->getMessage());
             DB::rollback();
-            return false;
+            return ['message' => __('messages.transfer.fail'), 'status' => JsonResponse::HTTP_INTERNAL_SERVER_ERROR];
         }
     }
 }
