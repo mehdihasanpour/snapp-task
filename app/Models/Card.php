ea37dcbd16565c1aa412168b1316f97240048a0a
Mehdi Hasanpour <me.hasani1367@gmail.com>
2023-08-25 23:58:13 +0330

    add transactions relations to card model

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Card extends Model
 {
@@ -15,5 +16,15 @@ class Card extends Model
     public function account(): BelongsTo
     {
         return $this->belongsTo(Account::class);
+    }
+
+    public function sourceTransactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class,'source_card_id');
+    }
+
+    public function destinationTransactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class,'destination_card_id');
     }
 }
