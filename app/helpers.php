d6f5e153c02fce258be76df56df25743c54cc76b
Mehdi Hasanpour <me.hasani1367@gmail.com>
2023-08-25 13:26:55 +0330

    add test for persian and arabic number

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
-function convert2english($string) {
+function convert2english($string)
+{
     $newNumbers = range(0, 9);
     $persianDecimal = array('&#1776;', '&#1777;', '&#1778;', '&#1779;', '&#1780;', '&#1781;', '&#1782;', '&#1783;', '&#1784;', '&#1785;');
     $arabicDecimal = array('&#1632;', '&#1633;', '&#1634;', '&#1635;', '&#1636;', '&#1637;', '&#1638;', '&#1639;', '&#1640;', '&#1641;');
@@ -11,4 +12,15 @@ function convert2english($string) {
     $string =  str_replace($arabicDecimal, $newNumbers, $string);
     $string =  str_replace($arabic, $newNumbers, $string);
     return str_replace($persian, $newNumbers, $string);
+}
+
+function convert2arabic($string)
+{
+    $newNumbers = range(0, 9);
+    $arabic = array('٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩');
+    $persian = array('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹');
+
+    $string =  str_replace($newNumbers, $arabic, $string);
+
+    return str_replace($newNumbers, $persian, $string);
 }
--- tests/Feature/TransactionControllerTest.php
+++ tests/Feature/TransactionControllerTest.php
@@ -28,6 +28,7 @@ class TransactionControllerTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
+        Http::fake(['https://api.kavenegar.com/v1/*' => Http::response(['status' => 200, 'message' => 'successful'], 200)]);
         $this->sourceAccount = Account::factory()->create(['user_id' => $this->sourceUser = User::factory()->create()]);
         $this->sourceCard = Card::factory()->create(['account_id' => $this->sourceAccount->id, 'balance' => 30000]);
         $this->destinationAccount = Account::factory()->create(['user_id' => $this->destinationUser = User::factory()->create()]);
@@ -38,23 +39,18 @@ class TransactionControllerTest extends TestCase
     public function user_can_send_money_to_another_person()
     {
         Queue::fake();
-        Http::fake(['https://api.kavenegar.com/v1/*' => Http::response(['status' => 200, 'message' => 'successful'], 200)]);
-        $sourceAccount = Account::factory()->create(['user_id' => $sourceUser = User::factory()->create()]);
-        $sourceCard = Card::factory()->create(['account_id' => $sourceAccount->id, 'balance' => 30000]);
-        $destinationAccount = Account::factory()->create(['user_id' => $destinationUser = User::factory()->create()]);
-        $destinationCard = Card::factory()->create(['account_id' => $destinationAccount->id, 'balance' => 50000]);
 
         $response = $this->postJson(route('v1.transfer'), [
-            'source_card_number' => $sourceCard->card_number,
-            'destination_card_number' => $destinationCard->card_number,
+            'source_card_number' => $this->sourceCard->card_number,
+            'destination_card_number' => $this->destinationCard->card_number,
             'amount' => $transferAmount = 10000
         ]);
 
         $response->assertOk();
         $response->assertJson(['message' => __('messages.transfer.success')]);
         $this->assertDatabaseHas('transactions', [
-            'source_card_id' => $sourceCard->id,
-            'destination_card_id' => $destinationCard->id,
+            'source_card_id' => $this->sourceCard->id,
+            'destination_card_id' => $this->destinationCard->id,
             'amount' => $transferAmount,
         ]);
         $this->assertDatabaseHas('wages', [
@@ -62,11 +58,11 @@ class TransactionControllerTest extends TestCase
             'wage_amount' => Wage::AMOUNT,
         ]);
         $this->assertDatabaseHas('cards', [
-            'account_id' => $sourceAccount->id,
+            'account_id' => $this->sourceAccount->id,
             'balance' => 30000 - ($transferAmount + Wage::AMOUNT),
         ]);
         $this->assertDatabaseHas('cards', [
-            'account_id' => $destinationCard->id,
+            'account_id' => $this->destinationCard->id,
             'balance' => 50000 + $transferAmount,
         ]);
         Queue::assertPushed(SendSmsNotification::class);
@@ -151,5 +147,20 @@ class TransactionControllerTest extends TestCase
         );
 
         $response->assertStatus(JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function it_could_use_arabic_and_persian_card_number()
+    {
+        $response = $this->postJson(
+            route('v1.transfer'),
+            [
+                'source_card_number' => convert2arabic($this->sourceCard->card_number),
+                'destination_card_number' => $this->destinationCard->card_number,
+                'amount' => 10000,
+            ],
+        );
+
+        $response->assertOk();
     }
 }
