775b1dc9cbe82e463a58fc7e729eef4a67c53fe4
Mehdi Hasanpour <me.hasani1367@gmail.com>
2023-08-25 15:26:18 +0330

    refactor ghasedak sms client

--- app/Services/SmsService/Ghasedak.php
+++ app/Services/SmsService/Ghasedak.php
@@ -6,8 +6,6 @@ class Ghasedak implements SmsServiceInterface
 {
     public function sendSms(string $recipient, string $message): bool
     {
-        // Implement the logic to send an SMS using MySmsProvider API
-        // Example code here
-        return true; // Return true on successful sending
+        return true; 
     }
 }
